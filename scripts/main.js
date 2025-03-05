d7c82399a8f2e7afdce18829f148191ee93b6d05
AlekseyAkimovIL <132119336+AlekseyAkimovIL@users.noreply.github.com>
2025-03-05 23:00:28 +0200

    update main.js

---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+const menuBtn = document.querySelector('.burger-button');
+const menu = document.querySelector('.header__menu');
+
+menuBtn.addEventListener('click', () => {
+   menu.classList.toggle('menu--open');
+});
